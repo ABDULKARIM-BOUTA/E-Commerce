84e66af3748ed67df925a56bb5aa65e8294e896b
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-05-06 20:51:32 +0300

    testing permissions

--- orders/views.py
+++ orders/views.py
@@ -1,24 +1,23 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 from orders.serializers import OrderSerializer, OrderItemSerializer
 from orders.models import Order
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from django.views.generic import TemplateView
 
 # api views
 class AdminOrderListCreateAPIView(ListCreateAPIView):
     serializer_class = OrderSerializer
-    permission_classes = []
+    permission_classes = [IsAdminUser]
 
     def get_queryset(self):
-        user = self.request.user
-        if user.is_superuser:
-            return Order.objects.prefetch_related('items__product').all()
+        return Order.objects.prefetch_related('items__product').all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
 class UserOrderListCreateAPIView(ListAPIView):
     serializer_class = OrderSerializer
-    permission_classes = []
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
@@ -26,7 +25,7 @@ class UserOrderListCreateAPIView(ListAPIView):
 
 class OrderUpdateDeleteAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = OrderSerializer
-    permission_classes = []
+    permission_classes = [IsAuthenticated]
     lookup_field = 'order_id'
 
     def get_queryset(self):
