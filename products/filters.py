0474be5176eff18b0da875d1a1fe9a53567c4f15
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-05-07 15:07:21 +0300

    filter category

--- products/filters.py
+++ products/filters.py
@@ -2,10 +2,12 @@ import django_filters
 from products.models import Product
 
 class ProductFilters(django_filters.FilterSet):
+    category = django_filters.CharFilter(field_name='category__name', lookup_expr='icontains')
+
     class Meta:
         model = Product
         fields = {
             'name': ['icontains'],
             'price': ['exact', 'lt', 'gt', 'range'],
-            'category': ['exact']
+         #   'category': ['exact']
         }
