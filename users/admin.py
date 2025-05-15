59734474862d72b841ca1e30db88919b69b87e20
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-05-15 17:37:07 +0300

    update admin.py for users

--- users/admin.py
+++ users/admin.py
@@ -3,10 +3,8 @@ from users.models import User
 from django.contrib.auth.admin import UserAdmin
 
 class CustomUserAdmin(UserAdmin):
-    # Define the list display for the user list page in the admin interface
     list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active', 'is_superuser')
 
-    # Fields to be displayed in the user details page in the admin
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number', 'birthdate')}),
@@ -14,7 +12,6 @@ class CustomUserAdmin(UserAdmin):
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
 
-    # List of fields to display in the form for creating a user
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
@@ -22,8 +19,7 @@ class CustomUserAdmin(UserAdmin):
         }),
     )
 
-    search_fields = ('email', 'first_name', 'last_name')  # Search by email, first name, or last name
-    ordering = ('email',)  # Order by email in the admin interface
-
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
 
 admin.site.register(User, CustomUserAdmin)
