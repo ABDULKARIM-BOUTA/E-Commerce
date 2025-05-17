bbcd6671a4229c6cb06ec9f68d62177c61df25a9
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-05-17 21:05:14 +0300

    update category model

--- categories/models.py
+++ categories/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=40, unique=True)
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50)
+    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subcategories')
 
     def __str__(self):
         return self.name
