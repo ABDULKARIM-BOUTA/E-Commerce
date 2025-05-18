22838dd3fcf360fc8839296a090b76aad2d3c465
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-05-18 16:37:20 +0300

    update order and oredr items models

--- orders/models.py
+++ orders/models.py
@@ -11,6 +11,7 @@ class Order(models.Model):
         PENDING = 'Pending'
         CONFIRMED = 'Confirmed'
         CANCELLED = 'Cancelled'
+        DELIVERED = 'Delivered'
 
     class PaymentChoices(models.TextChoices):
         CASH = 'Cash On Delivery'
@@ -23,14 +24,16 @@ class Order(models.Model):
     products = models.ManyToManyField(Product, through='OrderItem', related_name='orders')
     address = models.TextField()
     payment_method = models.CharField(max_length=50, choices=PaymentChoices.choices)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(1)])
 
     def __str__(self):
         return f'#Order: {self.order_id} by {self.user}'
 
 class OrderItem(models.Model):
+    """used as an intermediate class that connects products and orders while adding extra details like quantity and item total price"""
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default=1)
 
     @property
     def item_total_price(self):
