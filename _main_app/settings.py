861f51d41296276bd82261dc83b026a2ef637d00
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-05-20 14:32:16 +0300

    create frontend with react

--- _main_app/settings.py
+++ _main_app/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = '_main_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend', 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- _main_app/urls.py
+++ _main_app/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,3 +32,7 @@ urlpatterns = [
     path('api/vendors/', include('vendors.urls')),
     path('api/wishlists/', include('wishlists.urls')),
 ]
+
+# to configure media files
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
