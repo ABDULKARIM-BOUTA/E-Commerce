14bbec9bf084fcba4cf41074cb50e35dd1f56a3c
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-05-21 13:42:53 +0300

    fixing catgeory.map error

--- frontend/src/components/redux/actions/categoriesActions.js
+++ frontend/src/components/redux/actions/categoriesActions.js
@@ -4,10 +4,10 @@ export const fetchCategories = () => async (dispatch) => {
     try {
         const response = await axios.get('http://127.0.0.1:8000/api/categories/');
         dispatch({
-            type: 'SET_PRODUCTS',
+            type: 'SET_ CATEGORIES',
             payload: response.data,
         });
     } catch (error){
-        console.error('Error  fetching products:', error);
+        console.error('Error  fetching categories:', error);
     }
 }
--- frontend/src/components/redux/actions/productsActions.js
+++ frontend/src/components/redux/actions/productsActions.js
@@ -4,10 +4,11 @@ export const fetchProducts = () => async (dispatch) => {
     try {
         const response = await axios.get('http://127.0.0.1:8000/api/products/');
         dispatch({
-            type: 'SET_CATEGORIES',
+            type: 'SET_ PRODUCTS',
             payload: response.data,
         });
     } catch (error){
-        console.error('Error  fetching categories:', error);
+        console.error('Error  fetching products:', error);
     }
 }
+
